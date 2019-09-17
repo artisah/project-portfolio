3e6a5a8a41659295ba33aa7c73ee0cfb781efcf6
Arti Sah <arti.yamini@gmail.com>
2019-09-17 11:31:22 -0400

    Add about me and skills section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 $(document).ready(function() {
+
+    // HEADER SLIDER
+
     $('#slides').superslides({
       animation: 'fade',
       play: 5000,
       pagination: false,
     });
+
+    // HEADER TITLE TEXT
+    var options = {
+      strings: ["Web Developer", "Software Engineer", "Software QA"],
+      typeSpeed: 150,
+      loop: true,
+      startDelay: 1000,
+      showCursor: false,
+      smartBackspace: false
+    }
+    
+    var typed = new Typed(".typed", options);
+
+
+    // SKILLS CAROUSEL
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:3
+              },
+              1000:{
+                  items:5
+              }
+        }
+  })
+
   });
